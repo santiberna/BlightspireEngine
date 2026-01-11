cad6788b623097a786fdf11c49d1677e216200f2
Santiago Bernardino <santibberna@gmail.com>
2026-01-11 21:51:34 +0100

    [Change] module declaration helper

--- cmake/code_modules.cmake
+++ cmake/code_modules.cmake
@@ -1,12 +1,14 @@
 
 
 # FUNCTION THAT DECLARES ALL THE DEFAULTS OF A MODULE
-function(module_default_init module)
+function(module_declare type)
+    get_filename_component(module ${CMAKE_CURRENT_SOURCE_DIR} NAME)
 
     file(GLOB_RECURSE public_headers CONFIGURE_DEPENDS "public/*.hpp")
     file(GLOB_RECURSE private_sources CONFIGURE_DEPENDS "private/*.cpp")
     file(GLOB_RECURSE private_headers CONFIGURE_DEPENDS "private/*.hpp")
 
+    add_library(module type)
     target_sources(${module} PUBLIC ${public_files} PRIVATE ${private_headers} PRIVATE ${private_sources})
     target_include_directories(${module} PUBLIC "public" PRIVATE "private")
 
@@ -23,7 +25,7 @@ function(module_default_init module)
         if (test_sources)
             target_sources(${module} PRIVATE ${test_sources})
             target_link_libraries(${module} PRIVATE GTest::gtest)
-            target_link_libraries(UnitTests PRIVATE $<LINK_LIBRARY:WHOLE_ARCHIVE,${module}>)
+            target_link_libraries(UnitTests PRIVATE ${module})
         endif ()
 
     endif ()
@@ -35,8 +37,10 @@ function(module_default_init module)
 endfunction()
 
 # FUNCTION THAT ADDS A MODULE TO THE MAIN ENGINE LIBRARY
-function(target_add_module target directory module)
-    message(STATUS "### - ${module} Module")
+function(target_add_module target directory)
+    get_filename_component(module ${directory} NAME)
+
     add_subdirectory(${directory})
-    target_link_libraries(${target} INTERFACE ${module})
+    message(STATUS "### - ${module} Module")
+    target_link_libraries(${target} PRIVATE ${module})
 endfunction()

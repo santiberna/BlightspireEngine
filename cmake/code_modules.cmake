2cc80d2fe571b711ca9b0db4705d8e2c8b549f0f
Santiago Bernardino <santibberna@gmail.com>
2026-01-11 23:16:07 +0100

    refactor/modules (#17)
    * [Change] module declaration helper
    
    * [Fixed] small issues with previous commit
    
    * [Fixed] all modules and cleaned some dependencies

--- cmake/code_modules.cmake
+++ cmake/code_modules.cmake
@@ -1,12 +1,14 @@
 
 
 # FUNCTION THAT DECLARES ALL THE DEFAULTS OF A MODULE
-function(module_default_init module)
+function(module_declare type)
+    get_filename_component(module ${CMAKE_CURRENT_SOURCE_DIR} NAME)
 
     file(GLOB_RECURSE public_headers CONFIGURE_DEPENDS "public/*.hpp")
     file(GLOB_RECURSE private_sources CONFIGURE_DEPENDS "private/*.cpp")
     file(GLOB_RECURSE private_headers CONFIGURE_DEPENDS "private/*.hpp")
 
+    add_library(${module} ${type})
     target_sources(${module} PUBLIC ${public_files} PRIVATE ${private_headers} PRIVATE ${private_sources})
     target_include_directories(${module} PUBLIC "public" PRIVATE "private")
 
@@ -35,8 +37,10 @@ function(module_default_init module)
 endfunction()
 
 # FUNCTION THAT ADDS A MODULE TO THE MAIN ENGINE LIBRARY
-function(target_add_module target directory module)
-    message(STATUS "### - ${module} Module")
+function(target_add_module target directory)
+    get_filename_component(module ${directory} NAME)
+
     add_subdirectory(${directory})
+    message(STATUS "### - ${module} Module")
     target_link_libraries(${target} INTERFACE ${module})
 endfunction()

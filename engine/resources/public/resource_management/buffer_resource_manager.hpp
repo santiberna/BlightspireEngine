102d095a8cfc79a25f715870a5a0fe84d86de720
Santiago Bernardino <santibberna@gmail.com>
2026-04-26 00:37:55 +0200

    Optimized buffer size and simplified

--- engine/resources/public/resource_management/buffer_resource_manager.hpp
+++ engine/resources/public/resource_management/buffer_resource_manager.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
+#include "enum_utils.hpp"
 #include "resource_manager.hpp"
 #include "resources/buffer.hpp"
 #include "slot_map/container.hpp"
-
-#include "enum_utils.hpp"
-#include "vma_include.hpp"
-#include "vulkan_include.hpp"
 
 #include <memory>
 
@@ -17,6 +14,7 @@ namespace bb
 
 enum class BufferFlags
 {
+    NONE = 0,
     TRANSFER_DST = 1 << 0,
     INDEX_USAGE = 1 << 1,
     VERTEX_USAGE = 1 << 2,
@@ -32,18 +30,16 @@ class BufferResourceManager
 {
 public:
     explicit BufferResourceManager(const std::shared_ptr<VulkanContext>& context);
+    ~BufferResourceManager();
 
     ResourceHandle<bb::Buffer> Create(VkDeviceSize size, bb::Flags<bb::BufferFlags> flags, const char* name);
 
-    const bb::Buffer* Access(const ResourceHandle<bb::Buffer>& handle) const
+    [[nodiscard]] const bb::Buffer* Access(const ResourceHandle<bb::Buffer>& handle) const
     {
         return m_storage.get(handle);
     }
 
-    void Destroy(const ResourceHandle<bb::Buffer>& handle)
-    {
-        m_storage.remove(handle);
-    }
+    void Destroy(ResourceHandle<bb::Buffer> handle);
 
 private:
     bb::SlotMap<bb::Buffer> m_storage {};
